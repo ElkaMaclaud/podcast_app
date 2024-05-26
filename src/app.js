f13773e219f71408d679251f62fa693d3431d14d
ElkaMaclaud <elkaMaclaud@gmail.com>
2024-05-26 20:37:21 +0300

    added validation form

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
-console.log("App working")
+import { isValid } from "./utils";
+import "./styles.css";
+// document.addEventListener("DOMContentLoaded", () => { - ЗДЕСЬ НЕ НУЖЕН!!!!!!!!!!!!!!!!
+const form = document.getElementById("form");
+const input = form.querySelector("#question-input");
+const button = form.querySelector("#button");
+
+form.addEventListener("submit", submitFormHandler);
+input.addEventListener("input", () => {
+  button.disabled = !isValid(input.value);
+});
+
+function submitFormHandler(event) {
+  event.preventDefault();
+
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON(),
+    };
+
+    button.disabled = true;
+    // Async request to server to save question
+    console.log("Question", question);
+
+    input.value = "";
+    input.className = "";
+    button.disabled = false;
+  }
+  console.log(input.value);
+}
